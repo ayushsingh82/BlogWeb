ba63c37ce6aa81416785cd18cdfaca331c54ad80
Ayush Singh <ayushsinghmi711@gmail.com>
2023-09-29 22:23:58 +0530

    appwrite authentication

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+   console.log(import.meta.env.REACT_APP_APPWRITE_URL);
+
+  return (
+    <>
+ <h1>Blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
